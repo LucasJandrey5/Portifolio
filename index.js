c7a0d8cd66c55b4aa448d64576dc669700631e72
Lucas Jandrey <lucasjandrey5@gmail.com>
2023-01-19 12:06:58 -0300

    Seções adicionadas

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const HideShowNav = () => {
   navOpened ? (navOpened = false) : (navOpened = true);
 
   const nav = document.querySelector("#nav");
-  console.log(navOpened ? "Opened" : "Closed");
 
   navOpened ? nav.classList.add("showNav") : nav.classList.remove("showNav");
 };
